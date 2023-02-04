ba6eea2df130d2429c4b58a97f340bdb0a7a2b78
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-02-04 22:16:31 +0300

    Completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React, { useState } from 'react';
+
+const EyesOnMe = () => {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    console.log('Good!');
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+    setFocused(false);
+  };
+
+  return (
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+};
+
+export default EyesOnMe;
